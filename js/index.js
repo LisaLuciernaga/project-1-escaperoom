03b16d40e500f0115ba33e78e2220eb285969ba0
Toni Soler <tonisoler1986@gmail.com>
2023-04-26 09:55:20 +0200

    chronometer done

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 window.addEventListener("load", function(){
     
-    //this part is not defined in HTML, maybe we should delete it
-const startButton= document.getElementsById("start-button")
-startButton.addEventListener("click", function(){
-    game.startGame();
-})
+    //Toni: this part is not defined in HTML, maybe we should delete it
+
+// const startButton= document.getElementsById("start-button")
+// startButton.addEventListener("click", function(){
+//     game.startGame();
+// })
 
 })
 
@@ -16,77 +17,6 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 
-//Print startScreen
-ctx.fillStyle = "brown";
-ctx.fillRect(0, 0, 1000, 550);
-ctx.fillStyle = "#000000";
-ctx.font="65px Georgia";
-ctx.fillText("hello",canvas.width/2, canvas.height/2);
-
-canvas.addEventListener("click", function(){
-    ctx.save();
-    ctx.clearRect(0,0, canvas.width, canvas.height);
-
-    // Print Room Canvas
-    ctx.fillStyle = "yellow";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#000000";
-    ctx.font="65px Georgia";
-    ctx.fillText("this is the room canvas", 200, 200); // test, remove later and add img
-
-    ctx.restore();
-});
-
-
-
-const clickableObjects = [
-    new ClickableObject (50, 100),
-    new ClickableObject (),
-];
-
-
-const game = {
-    objects: clickableObjects,
-    score: 0,
-    startGame (){
-        //display game screen, call start timer, display score
-    },
-     startTimer() {
-        let minutes = 0;
-        let seconds = 0;
-        const timerElement = document.getElementById("timer")
-    
-        let intervalId = setInterval(() => {
-            seconds++;
-    
-            if (seconds === 60) {
-                seconds = 0;
-                minutes++;
-            };
-    
-            let timeString = padZero(minutes) + ":" + padZero(seconds);
-           
-            timerElement.textContent = timeString;
-    
-            if (minutes === 5) {
-                clearInterval(intervalId);
-                return;
-            }
-        }, 1000);
-    
-        return intervalId;
-    },
-    
-     padZero(num) {
-        return num.toString().padStart(2, "0");
-    },
-    loose () {
-        //show pop up with aunt Mildred & text
-    },
-    win () {
-        //show pop up (your gran won riddle contest or so, img)
-    }
-}
 
 
 //Class objects
@@ -118,6 +48,57 @@ class ClickableObject {
     }
 }
 
+const clickableObjects = [
+    new ClickableObject (50, 100),
+    new ClickableObject (),
+];
+
+
+const game = {
+    objects: clickableObjects,
+    score: 0,
+    startGame (){
+        //display game screen, call start timer, display score
+    },
+     startTimer() {
+        let minutes = 0;
+        let seconds = 0;
+        const timerElement = document.getElementById("timer")
+    
+        let intervalId = setInterval(() => {
+            seconds++;
+    
+            if (seconds === 60) {
+                seconds = 0;
+                minutes++;
+            };
+    
+            let timeString = this.padZero(minutes) + ":" + this.padZero(seconds );
+           
+            timerElement.textContent = timeString;
+            console.log(seconds)
+            if (minutes === 5) {
+                clearInterval(intervalId);
+                return;
+            }
+        }, 50);
+    
+        return intervalId;
+    },
+    
+     padZero(num) {
+        return num.toString().padStart(2, "0");
+    },
+    loose () {
+        //show pop up with aunt Mildred & text
+    },
+    win () {
+        //show pop up (your gran won riddle contest or so, img)
+    }
+}
+
+
+
 //Class riddle
 class Riddle {
     constructor(name, question, img, solution){
@@ -139,3 +120,27 @@ class Riddle {
         //check if the solution, player entered matches riddle solution, return boolean
     }
 }
+
+//Print startScreen
+ctx.fillStyle = "brown";
+ctx.fillRect(0, 0, 1000, 550);
+ctx.fillStyle = "#000000";
+ctx.font="65px Georgia";
+ctx.fillText("hello",canvas.width/2, canvas.height/2);
+
+canvas.addEventListener("click", function(){
+    ctx.save();
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
+    // Print Room Canvas
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#000000";
+    ctx.font="65px Georgia";
+    ctx.fillText("this is the room canvas", 200, 200); // test, remove later and add img
+
+    ctx.restore();
+});
+
+//Start the timer
+game.startTimer()
