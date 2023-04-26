329614633022c39a9b0efff111ea559e12bbb6ad
Erika Balog <enbalog@gmail.com>
2023-04-26 09:33:17 +0200

    No changes

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 //For BootStrap
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+//<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+//<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 
 window.addEventListener("load", function(){
     //THIS IS THE PART OF THE CODE, WHERE WE DECLARE THINGS
