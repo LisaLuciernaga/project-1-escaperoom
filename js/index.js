9cad89c4cfdb56369305647da212c80aa3e75e40
Erika Balog <enbalog@gmail.com>
2023-04-26 20:35:02 +0200

    Changed and added some images, styled text on the start screen

--- js/index.js
+++ js/index.js
@@ -9,7 +9,11 @@ window.addEventListener("load", function(){
 
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
-    let startText = "You're visiting your grandma and she asked for your help with her riddle contest. She's a little confused and scattered the riddles all over her living room... (click anywhere to begin)"
+    let startText1 = "You're visiting your grandma and she asked for your help with her riddle contest.";
+    let startText2 = "She's a little confused and scattered the riddles all over her living room... (click anywhere to begin)";
+
+    let auntMildred = document.createElement("img");
+    auntMildred.src = "img/auntMildred.png";
 
     //create all images for clickable objects
     let dog = document.createElement("img");
@@ -38,9 +42,9 @@ window.addEventListener("load", function(){
     frames.src = "img/frames.png";
     frames.text = "Some old photos"; //Text to display when hovering mouse
     
-    let painting = document.createElement("img");
-    painting.src = "img/painting.png";
-    painting.text = "Grandma, when she was young & beautiful";
+    let picture = document.createElement("img");
+    picture.src = "img/picture.png";
+    picture.text = "Grandma, when she was young & beautiful";
     
     let wardrobe = document.createElement("img");
     wardrobe.src = "img/wardrobe.png";
@@ -49,6 +53,27 @@ window.addEventListener("load", function(){
     let comode = document.createElement("img");
     comode.src = "img/comode.png";
     comode.text = "Bachelor's chest"
+
+    let roundTable = document.createElement("img");
+    roundTable.src = "img/roundTable.png";
+
+    let vase = document.createElement("img");
+    vase.src = "img/vase.png";
+
+    let sideTable = document.createElement("img");
+    sideTable.src = "img/sideTable.png";
+
+    let denture = document.createElement("img");
+    denture.src = "img/denture.png";
+
+    let chandelier = document.createElement("img");
+    chandelier.src = "img/chandelier.png";
+
+    let suitcase = document.createElement("img");
+    suitcase.src = "img/suitcase.png";
+
+    let suitcasey = document.createElement("img");
+    suitcasey.src = "img/suitcasey.png";
 
 
     
@@ -116,9 +141,17 @@ window.addEventListener("load", function(){
 
             //Print non-clickable objects
             ctx.drawImage(wardrobe, 0, 70, 300, 450);
-            ctx.drawImage(painting, 600, 20, 170, 200);
-            ctx.drawImage(frames, 300, 30, 170, 200);
+            ctx.drawImage(picture, 700, 35, 200, 180);
+            ctx.drawImage(frames, 300, 40, 150, 160);
             ctx.drawImage(comode, 260, 300, 200, 150);
+            ctx.drawImage(roundTable, 600, 300, 100, 150);
+            ctx.drawImage(vase, 598, 210, 80, 100);
+            ctx.drawImage(sideTable, 650, 470, 100, 80);
+            ctx.drawImage(denture, 685, 450, 30, 40);
+            ctx.drawImage(chandelier, 470, -40, 120, 200);
+            ctx.drawImage(suitcase, 47, 40, 200, 90);
+            ctx.drawImage(suitcasey, 65, 28, 152, 70);
+
 
             //Print objects on top of background
             for (let i = 0; i < clickableObjects.length; i++){
@@ -188,12 +221,17 @@ window.addEventListener("load", function(){
 
 //THIS IS THE PART OF THE CODE, WHERE WE EXECUTE THINGS
 
-    //Print startScreen
-    ctx.fillStyle = "#9F8E72";
-    ctx.fillRect(0, 0, 1000, 550);
-    ctx.fillStyle = "#000000";
-    ctx.font="20px Georgia";
-    ctx.fillText(startText, 10, 50);
+//Print startScreen
+ctx.fillStyle = "#9F8E72";
+ctx.fillRect(0, 0, 1000, 550);
+ctx.fillStyle = "#000000";
+ctx.font ="30px Blackadder ITC";
+
+ctx.fillText(startText1, 20, 420);
+ctx.fillText(startText2, 20, 480);
+
+ctx.drawImage(auntMildred, 500, 250, 90, 80);
+
     
     canvas.addEventListener("click", game.startGame);
     canvas.addEventListener("click", (e)=>{
